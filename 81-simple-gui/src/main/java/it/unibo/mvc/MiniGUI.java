dab1d262e2301a1c6d88a7283e58d1cfb8807c95
Emanuele Vendemini <emanuele.vendemini@studio.unibo.it>
2025-11-11 18:51:48 +0100

    81 finished

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,10 +1,12 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.TextField;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,21 +31,33 @@ public class MiniGUI {
      * Creates a new {@link MiniGUI}.
      */
     public MiniGUI() {
-        final JPanel canvas = new JPanel();
-        canvas.setLayout(new BorderLayout());
+        final JPanel outer = new JPanel();
+        outer.setLayout(new BorderLayout());
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
-        frame.setContentPane(canvas);
+        
+        frame.setContentPane(outer);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        final JPanel inner = new JPanel();
+        inner.setLayout(new BoxLayout(inner, BoxLayout.X_AXIS));
+        outer.add(inner, BorderLayout.CENTER);
+        inner.add(write);
+
+        TextField txtf = new TextField("Result");
+        outer.add(txtf, BorderLayout.NORTH);
         /*
          * Handlers
          */
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                txtf.setText(Integer.toString(randomGenerator.nextInt()));
             }
         });
+
+        
+
     }
 
     private void display() {
