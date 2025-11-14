9f98d48e5adbda2e82f63a2329742c7667bd777b
emanuele <emanuele.vendemini@studio.unibo.it>
2025-11-14 11:49:12 +0100

    merge laptop

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,72 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+/**
+ * Death Note Implementation.
+ */
+public class DeathNoteImpl implements DeathNote {
+
+    /**
+     * Get rule number of @param ruleNumber.
+     */
+    @Override
+    public String getRule(final int ruleNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+    }
+
+    /**
+     * Write @param name in the Death Note.
+     */
+    @Override
+    public void writeName(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    /**
+     * Write Death @param cause.
+     */
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    /**
+     * Write Death @param details.
+     */
+    @Override
+    public boolean writeDetails(final String details) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    /**
+     * Get Death cause for person with @param name.
+     */
+    @Override
+    public String getDeathCause(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    /**
+     * Get Death details for person with @param name.
+     */
+    @Override
+    public String getDeathDetails(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    /**
+     * Get if this name is written in the Death Note.
+     */
+    @Override
+    public boolean isNameWritten(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,70 @@
-package it.unibo.deathnote;
 
+package it.unibo.deathnote;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+
+/**
+ * Tests the {@link DeatNoteImpl} class.
+ */
 class TestDeathNote {
+
+    private DeathNoteImpl deathNote;
+
+    
+    @BeforeEach
+    void setup()
+    {
+        deathNote = new DeathNoteImpl();
+    }
+    /**
+     * Test Boundaries
+     */
+    @Test
+    void testBoundsRules()
+    {
+        final IllegalArgumentException e = assertThrowsExactly(IllegalArgumentException.class, 
+            () -> {
+                deathNote.getRule(0);
+                deathNote.getRule(-1);
+            },
+            "Rule 0 and negatives rules do not exists in the Death Note"
+            );
+        final String message = e.getMessage();
+        assertNotNull(message, "Exception Message Null");
+        assertFalse(message.isBlank(), "Exception Message Empy");
+    }
+
+    /**
+     * Test Rules Validity in the Death Note
+     */
+    @Test
+    void testRulesValidity()
+    {
+        for (String rule : DeathNote.RULES) {
+            assertNotNull(rule);
+            assertFalse(rule.isBlank());
+        }
+    }
+
+    /**
+     * Test Human name written correctly in the Death Note
+     */
+    @Test
+    void testWriteName()
+    {
+        String name = "Nicola Stroffolino";
+        if(deathNote.isNameWritten(name))
+        {
+            
+        }
+    }
 
 }
